4101d9f1b6ed4f45e7b6043d47337af7dffbec52
Dom Clayton <dominic.clayton@parkingeye.co.uk>
2023-07-25 19:23:31 +0100

    whitespace changes

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "project_id" {
-    description = "The Azure DevOps project Id"
-    type = string
+  description = "The Azure DevOps project Id"
+  type        = string
 }
 
 variable "variable_group_name" {
-    description = "The name of the variable group, must be unique per library"
-    type = string
+  description = "The name of the variable group, must be unique per library"
+  type        = string
 }
 
 variable "variable_group_description" {
-    description = "The description of the variable group"
+  description = "The description of the variable group"
 }
 
 variable "variable_group_variables" {
-    description = "A map of variables to add to the variable group"
-    type = map(object({
-        name = string
-        value = string
-        is_secret = bool
-        secret_value = string
-    }))
+  description = "A map of variables to add to the variable group"
+  type = map(object({
+    name         = string
+    value        = string
+    is_secret    = bool
+    secret_value = string
+  }))
 }
--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
-    required_version = "~> 1.4.0"
-    required_providers {
-        azuredevops = {
-            source = "microsoft/azuredevops"
-            version = "=0.6.0"
-        }
+  required_version = "~> 1.4.0"
+  required_providers {
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = "=0.6.0"
     }
+  }
 }
