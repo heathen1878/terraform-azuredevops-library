f63fd9f10292c77564efb1083e7680e6cba0715e
Dom Clayton <dominic.clayton@parkingeye.co.uk>
2023-07-25 19:23:31 +0100

    whitespace changes

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 resource "azuredevops_variable_group" "variable_group" {
-    project_id = var.project_id
-    name = var.variable_group_name
-    description = var.variable_group_description
+  project_id  = var.project_id
+  name        = var.variable_group_name
+  description = var.variable_group_description
 
-    dynamic "variable" {
-        for_each = var.variable_group_variables
+  dynamic "variable" {
+    for_each = var.variable_group_variables
 
-        content {
-            name = variable.value["name"]
-            value = variable.value["value"]
-            is_secret = variable.value["is_secret"]
-            secret_value = variable.value["secret_value"]
-        }
+    content {
+      name         = variable.value["name"]
+      value        = variable.value["value"]
+      is_secret    = variable.value["is_secret"]
+      secret_value = variable.value["secret_value"]
     }
+  }
 }
