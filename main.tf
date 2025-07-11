982f02a561c1574b36bb6368bb2a7f854067f599
Dom Clayton <dominic.clayton@parkingeye.co.uk>
2025-07-11 14:56:04 +0100

    fixed changes

--- main.tf
+++ main.tf
@@ -1,23 +1,17 @@
 resource "azuredevops_variable_group" "variable_group" {
-<<<<<<< Updated upstream
-  project_id  = var.project_id
-  name        = var.variable_group_name
-  description = var.variable_group_description
-=======
   project_id   = var.project_id
   name         = var.variable_group_name
   description  = var.variable_group_description
   allow_access = var.allow_access
->>>>>>> Stashed changes
 
   dynamic "variable" {
     for_each = var.variable_group_variables
 
     content {
-      name         = variable.value["name"]
-      value        = variable.value["value"]
-      is_secret    = variable.value["is_secret"]
-      secret_value = variable.value["secret_value"]
+      name         = variable.value.name
+      value        = variable.value.value
+      is_secret    = variable.value.is_secret
+      secret_value = variable.value.secret_value
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -3,25 +3,6 @@ variable "project_id" {
   type        = string
 }
 
-<<<<<<< Updated upstream
-variable "variable_group_name" {
-  description = "The name of the variable group, must be unique per library"
-  type        = string
-}
-
-variable "variable_group_description" {
-  description = "The description of the variable group"
-}
-
-variable "variable_group_variables" {
-  description = "A map of variables to add to the variable group"
-  type = map(object({
-    name         = string
-    value        = string
-    is_secret    = bool
-    secret_value = string
-  }))
-=======
 variable "allow_access" {
   description = "Should this variable group be shared by all pipelines?"
   type        = bool
@@ -48,5 +29,4 @@ variable "variables" {
       secret_value = string
     }
   ))
->>>>>>> Stashed changes
 }
