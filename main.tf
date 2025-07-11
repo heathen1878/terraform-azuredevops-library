ec404e7d92dc9cbd412746d419f3d720bd4b0fb5
Dom Clayton <dominic.clayton@parkingeye.co.uk>
2025-07-11 20:52:47 +0100

    updated var names

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "azuredevops_variable_group" "variable_group" {
   project_id   = var.project_id
-  name         = var.variable_group_name
-  description  = var.variable_group_description
+  name         = var.group_name
+  description  = var.group_description
   allow_access = var.allow_access
 
   dynamic "variable" {
-    for_each = var.variable_group_variables
+    for_each = var.variables
 
     content {
       name         = variable.value.name
